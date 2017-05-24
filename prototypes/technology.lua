182be520ce40b935ac2364ccb2bc0ffad2cfedad
daid <daid303@gmail.com>
2017-05-25 00:56:53 +0200

    Add tech icons.

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -2,7 +2,8 @@ data:extend({
 {
     type = "technology",
     name = "fm-alien-capture",
-    icon = "__factorimon__/graphics/technology/alien-sensor.png",
+    icon = "__factorimon__/graphics/technology/alien-capture.png",
+    icon_size = 128,
     effects = {
         { type = "unlock-recipe", recipe = "fm-stun-magazine" },
         { type = "unlock-recipe", recipe = "fm-arena" },
@@ -22,7 +23,8 @@ data:extend({
 {
     type = "technology",
     name = "fm-alien-breeding",
-    icon = "__factorimon__/graphics/technology/alien-sensor.png",
+    icon = "__factorimon__/graphics/technology/alien-breeding.png",
+    icon_size = 128,
     effects = {
         { type = "unlock-recipe", recipe = "fm-breeder" },
         { type = "unlock-recipe", recipe = "fm-hatchery" },
@@ -43,6 +45,7 @@ data:extend({
     type = "technology",
     name = "fm-alien-sensor",
     icon = "__factorimon__/graphics/technology/alien-sensor.png",
+    icon_size = 128,
     effects = { { type = "unlock-recipe", recipe = "fm-alien-sensor" } },
     prerequisites = {"circuit-network", "fm-alien-capture"},
     unit =
